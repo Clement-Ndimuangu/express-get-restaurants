0459d78beae7e9bdf031fa04800df0ad90e28de1
Clement Ndimuangu <clement.ndimuangu@verizon.com>
2024-09-22 22:32:26 -0500

    done with day two work

--- src/app.js
+++ src/app.js
@@ -5,10 +5,33 @@ const db = require("../db/connection");
 
 //TODO: Create your GET Request Route Below: 
 
+app.use(express.json())
+app.use(express.urlencoded())
+
 app.get('/restaurants', async (req,res)=>{
     const restaurants = await Restaurant.findAll();
     res.json(restaurants)
 })
 
+app.get('/restaurants/:id', async(req,res)=>{
+    const id = req.params.id;
+    const restaurant = await Restaurant.findByPk(id)
+    res.json(restaurant)
+})
+
+
+app.post("/restaurants",async (req,res)=>{
+    const body = req.body;
+    const newRestaurant = await Restaurant.create(body) 
+    res.json(newRestaurant)
+})
+
+app.put('/restaurants/:id', async(req,res)=>{
+    const id = req.params.id;
+    const body = req.body;
+    const restaurantToUpdate = await Restaurant.findByPk(id)
+    const updatedRestaurant = await restaurantToUpdate.update(body)
+    res.json(updatedRestaurant)
+})
 
 module.exports = app;
